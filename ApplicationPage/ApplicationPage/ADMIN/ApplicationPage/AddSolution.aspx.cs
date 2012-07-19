223a79c5f68da49ad73ba9bf1561d2a7a5929975
Philip Stathis <philipstathis@gmail.com>
2012-07-19 15:19:19 -0400

    Initial

---
+++ ApplicationPage/ApplicationPage/ADMIN/ApplicationPage/AddSolution.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System.IO;
+using Microsoft.SharePoint.Administration;
+
+namespace ApplicationPage.Layouts.ApplicationPage
+{
+    public partial class AddSolution : LayoutsPageBase
+    {
+        public static string fileName = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                SafeDelete(Server.MapPath("~/") + fileName);
+                fileName = null;
+            }
+            status.Text = null;
+        }
+        protected void ReUploadRFP(object sender, EventArgs e)
+        {
+            try
+            {
+                rfplabel.Visible = false;
+                uploadnewrfp.Visible = false;
+                SafeDelete(Server.MapPath("~/") + fileName);
+                RFPFile.Visible = true;
+                Uploadrfpbtn.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+            }
+        }
+        protected void UploadRFP(object sender, EventArgs e)
+        {
+            try
+            {
+                fileName = Path.GetFileName(RFPFile.FileName);
+                if (!String.Equals(fileName.Substring(fileName.LastIndexOf(".")), ".wsp", StringComparison.OrdinalIgnoreCase))
+                    throw new Exception("This is not a wsp file");
+                RFPFile.SaveAs(Server.MapPath("~/") + fileName);
+                rfplabel.Text = fileName + " has been succesfully uploaded";
+                RFPFile.Visible = false;
+                Uploadrfpbtn.Visible = false;
+                rfplabel.Visible = true;
+                uploadnewrfp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+            }
+        }
+        protected void AddingSolution(object sender, EventArgs e)
+        {
+            try
+            {
+                SPSolution solutionadded = SPFarm.Local.Solutions.Add(Server.MapPath("~/") + fileName);
+                SPFarm.Local.Solutions.Ensure(solutionadded);
+                status.Text = fileName + " has been added to the solution store";
+                SafeDelete(Server.MapPath("~/") + fileName);
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+            }
+        }
+        private void SafeDelete(string p)
+        {
+            try
+            {
+                File.Delete(Server.MapPath("~/") + fileName);
+            }
+            catch
+            {
+                //Nothing to Delete
+            }
+        }
+    }
+}
---
+++ ApplicationPage/ApplicationPage/Features/AddSolnFeature/AddSolnFeature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f968055f-2350-4b7f-9b9d-2a7ae4b27adf" description="This is an aspx page that can add a .wsp to farm using the web browser" featureId="f968055f-2350-4b7f-9b9d-2a7ae4b27adf" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="Add Solution to Farm" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="3d7862c6-5b68-4f7e-9175-9218e03ad3db" />
+  </projectItems>
+</feature>
